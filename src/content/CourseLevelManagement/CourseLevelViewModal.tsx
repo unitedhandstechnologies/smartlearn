c0fc4b9e0fbee17799911990a5929366468a21e7
saranyas12 <saranya.s@unitedhands.cc>
2023-03-07 10:15:50 +0530

    Course level View

--- src/content/CourseLevelManagement/CourseLevelViewModal.tsx
+++ src/content/CourseLevelManagement/CourseLevelViewModal.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, makeStyles, Theme } from '@material-ui/core';
 import { DialogComp, DialogContentDetails } from 'src/components';
 import { useTranslation } from 'react-i18next';
 import { LANGUAGE_NAME } from 'src/Config/constant';
+import { API_SERVICES } from 'src/Services';
+import { toast } from 'react-hot-toast';
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -25,22 +27,40 @@ const CourseLevelViewModal = (props: Props) => {
   const { onClose, rowData } = props;
   const classes = useStyles();
   const { t, i18n } = useTranslation();
+  const [courseLevelData, setcourseLevelData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      setcourseLevelData([]);
+      const response: any = await (
+        API_SERVICES.courseLevelService.getCourseLevelById(
+          rowData?.id,
+        )
+      );
+        if (response?.data?.course_level_language) {
+          setcourseLevelData(response?.data?.course_level_language);
+        }
+    } catch (err) {
+      toast.error(err?.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   const renderDialogContent = () => {
     const contentDetails = [
-      { content: 'Course Id', value: rowData?.id },
+      { content: 'Course Level Id', value: rowData?.id },
       {
         content: 'Course Level',
         value: rowData?.course_level_name
       },
       {
         content: t('language'),
-        value:'English, Hindi, Gujarati'
-          // rowData?.language_id === 1
-          //   ? LANGUAGE_NAME[1]
-          //   : rowData?.language_id === 2
-          //   ? LANGUAGE_NAME[2]
-          //   : LANGUAGE_NAME[3]
+        value: courseLevelData.map((item)=>{
+          return LANGUAGE_NAME[item.language_id]
+        }).join(',')
       },
       {
         content: 'Sort No',
